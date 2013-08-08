302f787d9c2a559cd83c8d7c4c4534f2c0d2560f
William Lebel <lebelwilliam@gmail.com>
2013-08-08 12:49:11 -0400

    webAPI update.

--- src/main/java/me/ampayne2/Hunter/Hunter.java
+++ src/main/java/me/ampayne2/Hunter/Hunter.java
@@ -53,7 +53,7 @@ public class Hunter extends GamePlugin {
     public Boolean loadArena(Arena arena) {
         hunters.put(arena, new ArrayList<String>());
         civilians.put(arena, new ArrayList<String>());
-        ultimateGames.getJettyServer().getHandler().addHandler("/Hunter/" + arena.getName(), new WebKillHandler(this, arena));
+        ultimateGames.addAPIHandler("/Hunter/" + arena.getName(), new WebKillHandler(this, arena));
         return true;
     }
 
